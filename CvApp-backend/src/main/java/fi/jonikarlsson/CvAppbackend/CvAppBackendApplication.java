3d0ef93101fd04f5e0fa35f62018a5fc9a91dd78
Opiskelu <Opiskelu@Joni-MacBook-Pro.local>
2023-02-03 19:36:25 +0200

    init

---
+++ CvApp-backend/src/main/java/fi/jonikarlsson/CvAppbackend/CvAppBackendApplication.java
@@ -0,0 +1,13 @@
+package fi.jonikarlsson.CvAppbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvAppBackendApplication.class, args);
+	}
+
+}
---
+++ CvApp-backend/src/test/java/fi/jonikarlsson/CvAppbackend/CvAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.jonikarlsson.CvAppbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
